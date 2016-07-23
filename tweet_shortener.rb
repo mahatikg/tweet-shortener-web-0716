4f71f4b0242edffcebd2e415c76b6ce9e99799a7
Mahati Gollamudi <mahatigollamudi@gmail.com>
2016-07-23 15:07:02 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,61 @@
 # Write your code here.
+def dictionary
+	dict = {
+		"be" => "b",
+		"hello" => "hi",
+		"to" => "2",
+		"too" => "2",
+		"two" => "2",
+		"for" => "4",
+		"For" => "4",
+		"four" => "4",
+		"you" => "u",
+		"at" => "@",
+		"and" => "&"
+	}
+end
+
+
+def word_substituter(string)
+	array = string.split
+	array.collect! do |word|
+		if dictionary.keys.include?(word)
+			word = dictionary[word]
+		else
+			word = word
+		end
+  end
+	string = array.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+	array.collect! do |tweet|
+		puts word_substituter(tweet)
+	end
+end
+
+def selective_tweet_shortener(string)
+	if string.length < 140
+    string
+  else
+		array = string.split
+		array.collect! do |word|
+			if dictionary.keys.include?(word)
+				word = dictionary[word]
+			else
+				word = word
+			end
+		end
+		string = array.join(" ")
+	end
+end
+
+def shortened_tweet_truncator(string)
+	selective_tweet_shortener(string)
+	if string.length > 140
+		string[137..string.length] = "..."
+	else
+		string
+	end
+	string
+end
